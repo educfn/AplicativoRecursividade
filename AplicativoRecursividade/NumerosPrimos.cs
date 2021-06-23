08b64c39535e7ec9ae11a7ff2f055fd68e294c1d
Eduardo CFN <eduardo@verion.ind.br>
2021-06-23 10:35:55 -0300

    *Solução recém criada.
    *Classe 'NumerosPrimos' criada.
    
    *Método 'NumerosPrimos.descobrirPrimosDesteNumero' criado.

---
+++ AplicativoRecursividade/NumerosPrimos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicativoRecursividade
+{
+    class NumerosPrimos
+    {
+
+        public List<int> descobrirPrimosDesteNumero(int valor)
+        {
+            List<int> numerosPrimosDoValor = new List<int>;
+
+            return numerosPrimosDoValor;
+        }
+
+    }
+}
---
+++ AplicativoRecursividade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplicativoRecursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
